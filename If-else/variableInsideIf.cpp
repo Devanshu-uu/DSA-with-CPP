a539292732bdbd94772129c7242eff81d143cb2d
Devanshu <141940273+Devanshu-uu@users.noreply.github.com>
2026-07-01 02:07:15 +0530

    Add variableInsideIf example
    Add C++ example demonstrating variable scope within if-else conditions and the distinction between assignment (=) and comparison (==) operators.

---
+++ If-else/variableInsideIf.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main(){
+    // int  x=32;
+    // int x=0;
+    // float x=0.001;
+    // int x=-23;
+    // if (x) {
+    //     cout<< "True";
+    // }
+    // else {
+    //     cout<< "False";
+    // }
+
+
+    // int a=0;
+    int a=2;
+
+    if (a=0) cout<< "True";
+    else cout<<"False";
+
+}
