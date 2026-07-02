77c6ceba92599afe1cc7832de1fc9e6617b1b151
Devanshu <141940273+Devanshu-uu@users.noreply.github.com>
2026-07-03 02:46:57 +0530

    Add odd numbers divisible by 3 program
    Add a new C++ program (Loops/Question.cpp/OddNumberdivisibleby3.cpp) that prints all odd numbers divisible by 3 from 1 to 100. Also add the compiled executable (OddNumberdivisibleby3.exe) and update the existing 1to100.exe binary.

---
+++ Loops/Question.cpp/OddNumberdivisibleby3.cpp
@@ -0,0 +1,20 @@
+// HW: Print all odd numbers divisible by 3 
+// from 1 to 100
+
+#include <iostream>
+#include <climits>
+using namespace std;
+int main(){
+    for (int i=1; i<=100; i+=2){
+        if(i%3==0){
+            cout<<i<<" ";
+        }        
+    }
+
+
+
+
+
+  
+
+}
