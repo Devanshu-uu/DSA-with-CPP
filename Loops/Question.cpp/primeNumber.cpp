147e7460a01cc17cfa245184da7dfafd3716a084
Devanshu <141940273+Devanshu-uu@users.noreply.github.com>
2026-07-08 22:48:05 +0530

    Add factors finder and clean up prime number code
    Added new factors.cpp program to find all factors of a number using optimized sqrt approach. Cleared incomplete primeNumber.cpp to allow for fresh implementation.

--- Loops/Question.cpp/primeNumber.cpp
+++ Loops/Question.cpp/primeNumber.cpp
@@ -3,18 +3,7 @@ using namespace std;
 int main(){
 
 
-    int n;
-    cout<<"Enter a Number: ";
-    cin>> n;
-    int is_prime=0;
-
-    for(int i=2; i<=n-1;i++){
-        if(n%i==0) isprime=0
-        
-
-    
-
-    }
+   
    
 
 }
