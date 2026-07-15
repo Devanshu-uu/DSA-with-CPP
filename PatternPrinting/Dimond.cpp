f86aa9709465f7b58566e3a675d0493e0fe80315
Devanshu <141940273+Devanshu-uu@users.noreply.github.com>
2026-07-15 17:15:42 +0530

    Add Bridge pattern; simplify Diamond printing
    Add new Bridge pattern program (PatternPrinting/Bridge.cpp) and its compiled binary. Refactor PatternPrinting/Dimond.cpp to use a single loop with space/star counters (old two-loop code commented out) for clearer diamond printing; updated compiled Dimond.exe accordingly.

--- PatternPrinting/Dimond.cpp
+++ PatternPrinting/Dimond.cpp
@@ -9,25 +9,42 @@ int n;
 cout<<"Enter n: ";
 cin>>n;
 
-for(int i=1; i<=n;i++){
-    for(int j=1;j<=n-i;j++){
+// for(int i=1; i<=n;i++){
+//     for(int j=1;j<=n-i;j++){
+//         cout<<"  ";
+//     }
+//     for(int j=1;j<=2*i-1;j++){
+//         cout<<"* ";
+//     }
+//     cout<<endl;
+//     }
+// for(int i=1;i<=n-1;i++){
+//     for(int j=1;j<=i;j++){
+//         cout<<"  ";
+//     }
+//     for (int j=1;j<=2*(n-i)-1;j++){
+//         cout<<"* ";
+//     }
+//     cout<<endl;
+// }
+int space=n-1 , star=1;   
+for(int i=1;i<=2*n-1;i++){
+    for(int j=1;j<=space;j++){
         cout<<"  ";
     }
-    for(int j=1;j<=2*i-1;j++){
+    for(int j=1;j<=star;j++){
         cout<<"* ";
     }
-    cout<<endl;
+    if(i<n){
+        space--;
+        star+=2;
     }
-for(int i=1;i<=n-1;i++){
-    for(int j=1;j<=i;j++){
-        cout<<"  ";
-    }
-    for (int j=1;j<=2*(n-i)-1;j++){
-        cout<<"* ";
+    else{
+        space++;
+        star-=2;
     }
     cout<<endl;
 }
-    
 
 
 
