df4c4003efe8d0b2d5ca6f34f3ff41833a344569
Devanshu <141940273+Devanshu-uu@users.noreply.github.com>
2026-07-22 01:38:24 +0530

    Add block scope example for C++
    Demonstrates C++ scoping rules by showing variable scoping within for loops and if blocks. The example illustrates that variables declared in block scopes are not accessible outside their respective blocks.

---
+++ Functions/BlockScope.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    for (int i=1;i<=5;i++){
+        cout<<i<<" ";
+
+    }
+    // cout<<i;
+
+    if(5>4){
+        int x=3;
+    }
+
+    // cout<<x; 
+
+
+
+}
