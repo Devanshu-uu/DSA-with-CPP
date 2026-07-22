de5b23384e596ba3d1a3288ec426dd9e37f05242
Devanshu <141940273+Devanshu-uu@users.noreply.github.com>
2026-07-22 23:05:02 +0530

    Add variable shadowing scope examples
    Add two C++ programs demonstrating variable shadowing:
    
    - AccessingAShadowGlobalVariable.cpp: Shows how to access a shadowed global variable using the scope resolution operator (::)
    - NestedScopeShadowing.cpp: Demonstrates variable shadowing in nested block scopes

---
+++ Functions/NestedScopeShadowing.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+
+    int x=10;
+
+    {
+
+        cout<<x<<endl;
+        x=100;
+
+        cout<<x<<endl;
+
+    }
+    cout<<x;
+
+
+  
+
+
+}
