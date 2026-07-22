16c274f902c54860e82cd9429470aac0a8886d25
Devanshu <141940273+Devanshu-uu@users.noreply.github.com>
2026-07-23 00:57:47 +0530

    Add function overloading examples
    Add two C++ source files demonstrating function overloading concepts: basic overloading with different parameter counts (FunctionOverloading.cpp) and overloading with different parameter types (FunctionOverloadingPart2.cpp).

---
+++ Functions/FunctionOverloading.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void fun(){
+    cout<<"Hi"<<endl;
+}
+void fun(int x){
+    cout<<"Hi Good Morning"<<endl;
+}
+// void fun(int x=3){
+//     cout<<"Hi eve"<<endl; // give error
+// }
+int main(){
+
+    fun();
+   
+    
+
+  
+
+
+}
---
+++ Functions/FunctionOverloadingPart2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void fun(int x){
+    cout<<"Hi"<<endl;
+}
+void fun(double x){
+    cout<<"Hi Good Morning"<<endl;
+}
+void fun(char x){
+    cout<<"Hi eve"<<endl; 
+}
+int main(){
+
+    fun('d');
+   
+    
+
+  
+
+
+}
