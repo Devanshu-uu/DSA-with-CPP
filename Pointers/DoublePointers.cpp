5de2e2e0c78becfac0be71929b527d12ae6a045d
Devanshu <141940273+Devanshu-uu@users.noreply.github.com>
2026-07-23 03:12:38 +0530

    Add pointer concept examples
    Add three C++ examples demonstrating pointer concepts: double pointers, double pointer dereferencing, and null pointers.

---
+++ Pointers/DoublePointers.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int x=23;
+    int* p1=&x;
+    int** p2=&p1;
+
+    cout<<&x<<endl;
+    cout<<p1<<endl;
+
+    cout<<&p1<<endl;
+    cout<<p2<<endl;
+
+
+
+
+
+}
