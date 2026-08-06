7fbdb4d76734e278f10daee2574fe838a320fb29
Devanshu <141940273+Devanshu-uu@users.noreply.github.com>
2026-08-06 19:21:17 +0530

    Add simple C++ array example programs
    Add three C++ examples under Array/: GarbageValue.cpp (prints an uninitialized array element to demonstrate garbage values), OutputArray.cpp (iterates and prints a static array), and InputArray.cpp (incomplete stub for array input). Compiled binaries for GarbageValue and OutputArray were also added. InputArray.cpp requires completion/fix.

---
+++ Array/GarbageValue.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {    
+    int marks[4];
+    cout<<marks[0];
+     
+
+}
---
+++ Array/OutputArray.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {    
+    int marks[]={74,23,54,76,32};
+    for (int i=0 ;i<=(size(marks)-1);i++){ // Treverse or 
+        cout<<marks[i]<<endl;
+    }
+
+
+
+}
