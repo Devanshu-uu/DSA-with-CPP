2a72f004dc42fa804ba1cb02de9e882142e2fa16
Devanshu <141940273+Devanshu-uu@users.noreply.github.com>
2026-08-06 18:17:32 +0530

    arry

---
+++ Array/Array.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int marks[]={74,23,54,76,32};
+    cout<<marks<<endl;
+    cout<<marks[2]<<endl;
+    cout<<marks[4]<<endl;
+    
+    marks[4]=0;
+    cout<<marks[4]<<endl;
+
+
+
+}
