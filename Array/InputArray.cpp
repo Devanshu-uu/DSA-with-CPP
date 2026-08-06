eb40c2420be576012af5c07cc379afa1e2821966
Devanshu <141940273+Devanshu-uu@users.noreply.github.com>
2026-08-06 20:02:39 +0530

    e

--- Array/InputArray.cpp
+++ Array/InputArray.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
+
 using namespace std;
 int main() {    
-    int marks[]={74
+    int n;
+    cout<<"Enter n: ";
+    cin>>n;
+    int marks[n];
+
+    cout<<"Enter marks: ";
+
+    for(int i=0;i<n;i++){
+         cin>>marks[i];
+
+    }
+
+    for (int i=0 ;i<n;i++){ // Treverse or 
+        cout<<marks[i]<<endl;
+    }
+    for (int i=0 ;i<n;i++){ // Treverse or 
+        if(marks[i]<0) cout<<marks[i];
+    }
+
+    
 
 
 }
