7c4cf9e59875ba3a834f4c31e1e12ac3ab7d1fb2
Devanshu <141940273+Devanshu-uu@users.noreply.github.com>
2026-08-06 18:49:10 +0530

    Float

--- Array/Array.cpp
+++ Array/Array.cpp
@@ -2,12 +2,19 @@
 using namespace std;
 int main() {
     int marks[]={74,23,54,76,32};
-    cout<<marks<<endl;
-    cout<<marks[2]<<endl;
-    cout<<marks[4]<<endl;
+    // cout<<marks<<endl;
+    // cout<<marks[2]<<endl;
+    // cout<<marks[4]<<endl;
     
-    marks[4]=0;
-    cout<<marks[4]<<endl;
+    // marks[4]=0; // Update
+    // cout<<marks[4]<<endl;
+    
+    // cin>>marks[4]; // Input
+    // cout<<marks[4]<<endl;
+    cout<<size(marks)<<endl;
+    cout<<sizeof(marks)/4<<endl;
+    cout<<sizeof(marks)/sizeof(int)<<endl;
+    
 
 
 
---
+++ Array/FloatArray.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    float marks[]={74.2,23.23,54.3,76.11,32.00};
+    cout<<marks[2]<<endl;
+    cout<<marks[4]<<endl;
+
+
+
+
+}
