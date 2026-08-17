4a7cbe79aa5c9770a58e15da95684da0d6f52037
Devanshu <devanshumohriya@gmail.com>
2026-08-17 20:04:41 +0530

    d

--- Vectors/HowVectorWorks.cpp
+++ Vectors/HowVectorWorks.cpp
@@ -4,12 +4,28 @@ using namespace std;
 int main() {
     vector<int> v;
     v.push_back(1);
+    cout<<v.size()<<" "<<v.capacity()<<endl;
     v.push_back(2);
+    cout<<v.size()<<" "<<v.capacity()<<endl;
     v.push_back(3);
+    cout<<v.size()<<" "<<v.capacity()<<endl;
     v.push_back(4);
-    v.push_back(5);
+    cout<<v.size()<<" "<<v.capacity()<<endl;
+    v.pop_back();
+    cout<<v.size()<<" "<<v.capacity()<<endl;
+    v.pop_back();
+    cout<<v.size()<<" "<<v.capacity()<<endl;
     v.push_back(6);
-    for (int i=0;i<v.size();i++){
-        cout<<v[i]<<" ";
-    }
+    cout<<v.size()<<" "<<v.capacity()<<endl;
+
+    // vector<int> v;
+    // v.push_back(1);
+    // v.push_back(2);
+    // v.push_back(3);
+    // v.push_back(4);
+    // v.push_back(5);
+    // v.push_back(6);
+    // for (int i=0;i<size(v);i++){
+    //     cout<<v[i]<<" ";
+    // }
 }
