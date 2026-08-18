f6c7060fdef37bc46bf5d6eab11064335c82d294
Devanshu <devanshumohriya@gmail.com>
2026-08-19 00:55:26 +0530

    Create MissingNumberInArray.cpp

---
+++ Vectors/VectorQuestions/MissingNumberInArray.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        
+        for (int i=0;i<=nums.size();i++){
+            bool flag= false;
+            for (int elem :nums){
+                if(elem==i){
+                    flag=true;
+                    break;
+                }}
+                if(flag==false) return i;
+
+        
+
+
+
+         
+    }
+     return 0;
+    }
+   
+};
