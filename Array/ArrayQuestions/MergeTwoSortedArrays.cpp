b9e60498c91e59968ed3b6fd6b629ac51a3bd24e
Devanshu <141940273+Devanshu-uu@users.noreply.github.com>
2026-08-21 02:42:03 +0530

    mergeArray

---
+++ Array/ArrayQuestions/MergeTwoSortedArrays.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include<algorithm>
+using namespace std;
+int main() {
+    int a[]={10,20,40,70,90,100};
+    int b[]={30,50,60,80};
+    int m=size(a),n=size(b);
+    int c[m+n];
+    int i=0,j=0,k=0;
+
+    while(i<m && j<n ){
+        if(a[i]<b[j]) 
+        c[k++]=a[i++];
+        else 
+        c[k++]=b[j++];
+           
+        
+    }
+
+    while(i<m) 
+    c[k++]=a[i++];
+    while(j<n)
+    c[k++]=b[j++];
+
+
+    for(int i=0;i<m+n;i++){
+        cout<<c[i]<<" ";
+    }
+    
+}
