fbd3701baea2171a70432c3ed415c387c99d2ae3
Devanshu <141940273+Devanshu-uu@users.noreply.github.com>
2026-08-21 20:31:17 +0530

    s

---
+++ 2DArray/tempCodeRunnerFile.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int start=0 ,end=5;
+    int arr[][5]={
+     {6,0,9,2,3}
+    ,{5,2,6,0,9}
+    ,{8,4,8,1,7},
+    {4,1,7,3,6}};
+    bool flag=false;
+
+    for (int i=0;i<4;i++){
+
+        if(flag){
+            for(int j=start;j>end;j--){
+            cout<<arr[i][j]<<" ";
+        }
+    }
+        else{
+            for(int j=start;j<end;j++){
+            cout<<arr[i][j]<<" ";
+           
+        }
+        
+
+        }
+        
+  if(flag){
+            start=0;
+            end=5;
+            flag=false;
+        }
+        else{
+            
+            start=4;
+            end=-1;
+            flag=true;
+        }
+    }
+
+}
+
+
+
+
+
