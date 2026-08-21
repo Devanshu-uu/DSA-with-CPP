5993f9df93427c8b5eedd4530b46270a44bbce51
Devanshu <141940273+Devanshu-uu@users.noreply.github.com>
2026-08-21 18:35:59 +0530

    g

--- 2DArray/Sum2DArray.cpp
+++ 2DArray/Sum2DArray.cpp
@@ -3,16 +3,23 @@ using namespace std;
 int main (){
     // int arr[3][4];
     int  sum=0;
-    int arr[][4]={{5,3,6,1},{9,9,4,4},{5,6,1,0}};
+    int mx=INT_MIN;
+    int row=0;
+    int arr[][4]={{9,9,9,9},{9,9,4,4},{5,6,1,0}};
     
-    for (int j=0;j<4;j++){
-        for (int i=0;i<3;i++){
+    for (int i=0;i<3;i++){
+        for (int j=0;j<4;j++){
             sum+=arr[i][j];
+            
         }
-        cout<<endl;
+        if (sum>mx){
+            row=i;
+            mx=sum;}
+        sum=0;
+        
 
-// }
-cout<<sum<<endl;
+
 }  
+cout<<row<<endl;
 
 }
