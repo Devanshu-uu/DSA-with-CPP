67708c45732a17b796c3affd0c7793ff12218c0f
Devanshu <141940273+Devanshu-uu@users.noreply.github.com>
2026-08-25 02:54:14 +0530

    2d

---
+++ 2DArray/Transpose.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int arr[][4]={
+     {1,2,3,4}
+    ,{1,2,3,4}
+    ,{1,2,3,4},
+    {1,2,3,4}};
+
+    for(int i=0;i<4;i++){
+        for(int j=0;j<i;j++){
+            swap(arr[i][j],arr[j][i]);
+        }
+    }
+    for(int i=0;i<4;i++){
+        for(int j=0;j<4;j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    
+
+  
+  
+
+
+
+
+}
+
+
+
+
+
