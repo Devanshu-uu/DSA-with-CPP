293bbe131d76bae0026ee9214a718f43f5e3e947
Devanshu <141940273+Devanshu-uu@users.noreply.github.com>
2026-08-25 22:49:28 +0530

    f

--- Vectors/2D Vectors/PascalTriangle.cpp
+++ Vectors/2D Vectors/PascalTriangle.cpp
@@ -1,3 +1,25 @@
+// class Solution {
+// public:
+//     vector<vector<int>> generate(int numRows) {
+//         vector<vector<int>> arr;
+//         for(int i=1;i<=numRows;i++){
+//             arr.push_back(vector<int>(i,1));
+            
+//         }
+//         for(int i=0;i<arr.size();i++){
+//             for(int j=0;j<arr[i].size();j++){
+//                 if(i!=j && j!=0){
+//                     arr[i][j]=arr[i-1][j-1]+arr[i-1][j];
+//                 }
+//             }
+//         }
+//         return arr;
+        
+//     }
+// };
+
+
+
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
@@ -6,14 +28,13 @@ public:
             arr.push_back(vector<int>(i,1));
             
         }
-        for(int i=0;i<arr.size();i++){
-            for(int j=0;j<arr[i].size();j++){
-                if(i!=j && j!=0){
-                    arr[i][j]=arr[i-1][j-1]+arr[i-1][j];
+        for(int i=2;i<arr.size();i++){
+            for(int j=1;j<arr[i].size()-1;j++){
+                arr[i][j]=arr[i-1][j-1]+arr[i-1][j];
                 }
             }
-        }
         return arr;
+        }
         
-    }
+    
 };
