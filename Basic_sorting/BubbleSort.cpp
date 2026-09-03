b39ccff9d1cc718eb03b845ee9107eecd0c6c721
Devanshu <141940273+Devanshu-uu@users.noreply.github.com>
2026-09-04 00:19:38 +0530

    tg

---
+++ Basic_sorting/BubbleSort.cpp
@@ -0,0 +1,31 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    vector<int> arr={5,4,3,2,1,6};
+    int n=arr.size();
+    for(int j=0;j<n-1;j++){
+        int swaps=0;
+        for(int i=0;i<n-j-1;i++){
+            if(arr[i]>arr[i+1]) {
+                swap(arr[i],arr[i+1]);
+                swaps++;
+        }
+        if(swaps==0) break;
+    }
+    
+    
+}
+for(int i=0;i<arr.size()-1;i++){
+    cout<<arr[i]<<"";
+}
+
+}
+
+    
+    
