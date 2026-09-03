156b84520b70c3cc962fcbcdfb85f71ea2717879
Devanshu <141940273+Devanshu-uu@users.noreply.github.com>
2026-09-04 02:42:13 +0530

    s

--- Basic_sorting/SelectionSort.cpp
+++ Basic_sorting/SelectionSort.cpp
@@ -4,9 +4,36 @@
 using namespace std;
 
 int main() {
-    vector<int> arr = {9,-2,0,0,-4,6,0,7,0};
+    vector<int> arr = {7,4,9,1,3,6,2,5};
     int n=arr.size();
     
+    for(int i=0;i<n-1;i++){
+        int mn=arr[i];
+        int index=i;
+        for(int j=i;j<n ;j++)
+        {
+            if(arr[j]<mn){
+            index=j;
+            mn=arr[j];
+
+        } 
+        
+        
+    }
+    swap(arr[index],arr[i]);
+
+    }
+        
+        
+    
+        
+    
+    for(int i=0;i<=arr.size()-1;i++){
+    cout<<arr[i]<<"";
+}
+    
+
+
 
 
 
