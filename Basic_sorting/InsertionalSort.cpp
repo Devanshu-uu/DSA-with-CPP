610819b11dc3d64d18844496eb81e889fabcef4d
Devanshu <141940273+Devanshu-uu@users.noreply.github.com>
2026-09-04 16:30:44 +0530

    Create InsertionalSort.cpp

---
+++ Basic_sorting/InsertionalSort.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    vector<int> arr = {9,-2,0,0,-4,6,0,7,0};
+    int n=arr.size();
+    
+   
+}
